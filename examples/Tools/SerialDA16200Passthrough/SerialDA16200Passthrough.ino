20ba8643d3f494f95113447a1314d2a5088aff89
Sandeep Mistry <sandeep.mistry@arm.com>
2021-10-08 13:26:49 -0400

    Initial library structure and serial pass through example

---
+++ examples/Tools/SerialDA16200Passthrough/SerialDA16200Passthrough.ino
@@ -0,0 +1,25 @@
+int resetPin = 7;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial); // wait for the serial monitor to be opened
+
+  SERIAL_PORT_HARDWARE_OPEN.begin(115200);
+
+  // reset the DA16200 module
+  pinMode(resetPin, OUTPUT);
+  digitalWrite(resetPin, LOW);
+  delay(100);
+  digitalWrite(resetPin, HIGH);
+  delay(100);
+}
+
+void loop() {
+  if (Serial.available()) {
+    SERIAL_PORT_HARDWARE_OPEN.write(Serial.read());
+  }
+
+  if (SERIAL_PORT_HARDWARE_OPEN.available()) {
+    Serial.write(SERIAL_PORT_HARDWARE_OPEN.read());
+  }
+}
---
+++ src/DA16200_WiFi.h
@@ -0,0 +1,4 @@
+#ifndef _DA16200_WIFI__H_
+#define _DA16200_WIFI__H_
+
+#endif
